5b027410816d116fb2bd446f48feeae47d6d4ec9
Miguel Montanez <MAMontanez@minambiente.gov.co>
2024-09-15 23:22:11 -0500

    unal experience

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,17 +1,10 @@
-// Create the template element
 const template = document.createElement('template');
 
-// Fetch the content of header.html
 fetch('../../header.html')
-  .then(response => response.text())  // Get the response as text
+  .then(response => response.text())  
   .then(html => {
-    // Set the template's innerHTML with the fetched HTML
     template.innerHTML = html;
-
-    // Get the placeholder element where you want to insert the template
     var headerTemplate = document.getElementById('headerTemplate');
-
-    // Append the template content into the placeholder
     headerTemplate.appendChild(template.content);
   })
   .catch(error => {
